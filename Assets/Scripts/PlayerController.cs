5d91f8918b4b9383a1e03d8d6272ca058ec946a9
n.dremov <pmi22dremov@gmail.com>
2023-05-15 13:12:01 +0400

    f[sit]: implement sitting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,77 +1,113 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public float height = 1;
-    public float highHeight = 1;
-    public float rotationSpeed = 100;
-    public float walkingSpeed = 1;
-    public float runningSpeed = 2;
-    public float jumpForce = 1;
+    private float horizontalTurning => Input.GetAxis("Mouse X");
+    private float verticalTurning => Input.GetAxis("Mouse Y");
 
-    public Vector3 Gravity;
+    private float horizontalMoving => Input.GetAxis("Horizontal");
+    private float verticalMoving => Input.GetAxis("Vertical");
 
-    private Vector3 moveDirection;
+    public KeyCode RunningKey = KeyCode.LeftShift;
+    private bool isRunningPressed => Input.GetKey(RunningKey);
+
+    public KeyCode JumpingKey = KeyCode.Space;
+    private bool isJumpingPressed => Input.GetKey(JumpingKey);
+
+    public KeyCode SittingKey = KeyCode.LeftControl;
+    private bool isSittingPressed => Input.GetKey(SittingKey);
+
     private CharacterController _controller;
 
-    private bool isRunning = false;
-    private bool isJumping = false;
+    public Vector3 Gravity;
+    public float Height = 1;
+    public float JumpSpeed = 1;
 
-    private float horizontalTurning = 0;
-    private float verticalTurning = 0;
+    private bool IsGrounded => Physics.Raycast(transform.position, -Vector3.up, Height);
+    private Vector3 _speed;
+    private Vector3 gravityAcceleration => IsGrounded ? Vector3.zero : Gravity;
+    private Vector3 acceleration => gravityAcceleration;
 
-    private float horizontalMoving = 0;
-    private float verticalMoving = 0;
+    public float crowdSpeed = 0.5f;
+    public float walkingSpeed = 1;
+    public float runningSpeed = 2;
+    private float speedForUse => isSitting ? crowdSpeed : isRunningPressed ? runningSpeed : walkingSpeed;
+    private Vector3 forwardSpeed => -new Vector3(
+           transform.forward.x * verticalMoving,
+           0,
+           transform.forward.z * verticalMoving
+        ) * speedForUse;
+    private Vector3 sideSpeed => -new Vector3(
+           transform.right.x * horizontalMoving,
+           0,
+           transform.right.z * horizontalMoving
+        ) * speedForUse;
+    private Vector3 movementSpeed => forwardSpeed + sideSpeed;
+    private bool isSitting = false;
 
-    bool IsGrounded => Physics.Raycast(transform.position, -Vector3.up, height);
+    private float originalHeight = 0;
+    // ============================================
 
-    bool IsHighGrounded => Physics.Raycast(transform.position, -Vector3.up, highHeight);
-    bool startJump = false;
-    public void OnRunning()
-    {
-        isRunning = true;
-    }
-    public void OnUnrunning()
-    {
-        isRunning = false;
-    }
 
-    public void OnJumping()
-    {
-        isJumping = true;
-    }
-    public void OnUnjumping()
-    {
-        isJumping = false;
-    }
-    public void OnHorizontalTurning(float horizontalTurning)
-    {
-        this.horizontalTurning = horizontalTurning;
-    }
-    public void OnVerticalTurning(float verticalTurning)
-    {
-        this.verticalTurning = verticalTurning;
-    }
-    public void OnHorizontalMoving(float horizontalMoving)
-    {
-        this.horizontalMoving = horizontalMoving;
-    }
-    public void OnVerticalMoving(float verticalMoving)
-    {
-        this.verticalMoving = verticalMoving;
-    }
+    public float rotationSpeed = 100;
+    private Vector3 moveDirection;
+
+
+
+
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         _controller = GetComponent<CharacterController>();
+        originalHeight = _controller.height;
     }
 
     void Update()
     {
-        Debug.Log(IsGrounded);
         CalculateTurning();
-        CalculateMovement();
+        UpdateMovement();
+        UpdateSitting();
+    }
+    void UpdateMovement()
+    {
+        _speed += acceleration * Time.deltaTime;
+        if (IsGrounded)
+        {
+            if (!isSitting)
+            {
+                if (_speed.y < 0)
+                {
+                    _speed.y = 0;
+                }
+                if (isJumpingPressed)
+                {
+                    _speed.y = JumpSpeed;
+                }
+            }
+
+            var moveSpeed = movementSpeed;
+            _speed.x = moveSpeed.x;
+            _speed.z = moveSpeed.z;
+        }
+
+        _controller.Move(_speed * Time.deltaTime);
+    }
+    void UpdateSitting()
+    {
+        if (IsGrounded && isSittingPressed && !isSitting)
+        {
+            isSitting = true;
+            _controller.height = originalHeight / 2;
+            _controller.Move(Vector3.down * originalHeight / 4);
+        }
+        else if (!isSittingPressed && isSitting)
+        {
+            isSitting = false;
+            _controller.height = originalHeight;
+            _controller.Move(Vector3.up * (originalHeight / 2 - 0.5f));
+        }
     }
 
     void CalculateTurning()
@@ -85,45 +121,5 @@ public class PlayerController : MonoBehaviour
             moveDirection += targetFlyRotation * rotationSpeed;
             transform.rotation = Quaternion.LookRotation(moveDirection);
         }
-    }
-
-    void CalculateMovement()
-    {
-        var horizontalVelocity = Vector3.zero;
-        var verticalVelocity = Vector3.zero;
-        var jumpVelocity = Vector3.zero;
-            var speed = isRunning ? runningSpeed : walkingSpeed;
-            horizontalVelocity = -new Vector3(
-               transform.right.x * horizontalMoving,
-               0,
-               transform.right.z * horizontalMoving
-            ) * speed;
-            verticalVelocity = -new Vector3(
-               transform.forward.x * verticalMoving,
-               0,
-               transform.forward.z * verticalMoving
-            ) * speed;
-
-        if (IsGrounded)
-        {
-            if (isJumping && !startJump)
-            {
-                startJump = true;
-            }
-        }
-
-        if (startJump)
-        {
-            if (!IsHighGrounded)
-            {
-                startJump = false;
-            }
-            jumpVelocity = new Vector3(
-              0,
-              jumpForce,
-              0
-           );
-        }
-        _controller.Move((Gravity + horizontalVelocity + verticalVelocity + jumpVelocity) * Time.deltaTime);
     }
 }
