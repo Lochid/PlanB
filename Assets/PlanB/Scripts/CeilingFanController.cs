f18aa4f60b63827a534b08b6559d9b9d0e2c27e3
Lochid <pmi22dremov@gmail.com>
2023-06-30 15:27:16 +0400

    F/ambient music (#11)

---
+++ Assets/PlanB/Scripts/CeilingFanController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingFanController : MonoBehaviour
+{
+    public Animator fanAnimator;
+    public bool turnedOn { get; private set; } = false;
+
+    public List<AudioSource> turnOnSounds;
+    public List<AudioSource> turnOffSounds;
+    public List<AudioSource> workingSounds;
+    public void Turn()
+    {
+        SetPower(!turnedOn);
+    }
+    public void SetPower(bool power)
+    {
+        turnedOn = power;
+        fanAnimator?.SetBool("Powered", power);
+        if (turnedOn)
+        {
+            foreach (var sound in turnOnSounds)
+            {
+                sound.Play();
+            }
+            foreach (var sound in workingSounds)
+            {
+                sound.PlayDelayed(2.5f);
+            }
+        }
+        else
+        {
+            foreach (var sound in workingSounds)
+            {
+                sound.Stop();
+            }
+            foreach (var sound in turnOffSounds)
+            {
+                sound.Play();
+            }
+        }
+    }
+}
